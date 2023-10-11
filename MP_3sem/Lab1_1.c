5d51f1050e870f03a5f6eeacbc1c0cbb641c6fe0
rob228rob <rob228rob@gmail.com>
2023-10-11 16:23:20 +0300

    Lab1_1 last upd(i hope)

--- MP_3sem/Lab1_1.c
+++ MP_3sem/Lab1_1.c
@@ -29,6 +29,10 @@ void get_multiples_nums(int current_num, int** array_answ) {
     (*array_answ) = NULL;
     return;
   }
+  if (current_num > 50) {
+    (*array_answ) = NULL;
+    return;
+  }
   (*array_answ) = (int*)malloc(sizeof(int)*(100/current_num + 1));
   int index = 0;
   for (int i = current_num + current_num; i <= 100; i += current_num) {
@@ -43,11 +47,11 @@ void print_array(int* array_answ, int length) {
   }
 
   for (int i = 0; i < length; ++i) {
-    if (array_answ[i] != 0) {
-      printf("%d \n", array_answ[i]);
+    if (1) {
+      printf("%d ", array_answ[i]);
     }
-
   }
+  printf("\n");
 }
 
 bool is_prime(int current_num) {
@@ -70,6 +74,9 @@ void cut_number(char string_num[], int** array_answ, int* count) {
   
   bool has_zero = true;
   for (int i = 0; i < length; ++i) {
+    if (string_num[i] != '0') {
+      break;
+    }
     if (string_num[i] == '0' && has_zero) {
       start_ind++;
     }
@@ -86,6 +93,10 @@ void cut_number(char string_num[], int** array_answ, int* count) {
 }
 
 int sum_to_num(int current_num) {
+  if (current_num > 65500) {
+    return -1;
+  }
+ 
   return (double)(1 + current_num) / 2 * current_num;
 }
 
@@ -116,13 +127,14 @@ void calculating_degree(int degree, int* res_arr) {
 }
 
 void make_table(int current_num) {
+  int* result_arr;
+  current_num++;
   for (int i = 1; i < current_num; ++i) {
-    int* result_arr = (int*)malloc(sizeof(int)*10);
+    result_arr = (int*)calloc(10, sizeof(int));
     calculating_degree(i, result_arr);
     printf("degree:\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\n");
     printf("value:\t");
     for (int i = 0; i < 10;++i) {
-      
       if (i != 9) {
         printf("%d\t", result_arr[i]);
       } else {
@@ -130,7 +142,10 @@ void make_table(int current_num) {
       }
     }
     printf("\n");
+
   }
+  free(result_arr);
+  return;
 }
 
 int main(int argc,char* argv[]) {
@@ -148,12 +163,14 @@ int main(int argc,char* argv[]) {
     flag_component = argv[1][0];
     flag = argv[1][1];
     current_num = atoi(argv[2]);
-    string_num = argv[2];
+    string_num = (char*)malloc(sizeof(char)*strlen(argv[2]) + 1);
+    strcpy(string_num, argv[2]);
   } else {
     flag_component = argv[2][0];
       flag = argv[2][1];
       current_num = atoi(argv[1]);
-      string_num = argv[1];
+      string_num = (char*)malloc(sizeof(char)*strlen(argv[2]) + 1);
+      strcpy(string_num, argv[2]);
   }
   
   if ((flag_component != '-' && flag_component != '/') || (!is_flag(flag))) {
@@ -161,7 +178,7 @@ int main(int argc,char* argv[]) {
     return 0;
   }
 
-    if (!is_num(string_num)) {
+  if (!is_num(string_num)) {
     printf("Incorrect number.\n");
     return 0;
   }
@@ -180,14 +197,19 @@ int main(int argc,char* argv[]) {
     if (array_answ != NULL) {
       print_array(array_answ, 100/current_num + 1);
     } else {
-      printf("Isnt number multiples to current.\n");
+      printf("There's not any number multiples to current.\n");
+      free(array_answ);
+      free(string_num);
+      return 1;
     }
-    free(array_answ);
+
     break;
+
   case 'p':
     if (current_num <= 1) {
       printf("Incorrect number for this flag.\n");
-      return 0;
+      free(string_num);
+      return 1;
     }
     temp = is_prime(current_num);
     if (temp) {
@@ -204,23 +226,32 @@ int main(int argc,char* argv[]) {
     } else {
       printf("Something went wrong.\n");
     }
-    if (array_answ != NULL) free(array_answ);
+    free(array_answ);
+    free(string_num);
     break;
   case 'e':
-    if (current_num > 10) {
+    if (current_num > 10 || current_num < 2) {
       printf("Invalid number.\n");
       return 0;
     }
     make_table(current_num);
+    free(string_num);
     break;
   case 'a':
     summ = sum_to_num(current_num);
+    if (sum_to_num != -1) {
+      printf("Too large number.\n");
+      free(string_num);
+      return 1;
+    }
     printf("%lld - sum up to number.\n", summ);
+    free(string_num);
     break;
   case 'f':
     if (current_num >= 20 || current_num < 0) {
       printf("Isnt valid num for this flag.\n");
-      return 0;
+      free(string_num);
+      return 1;
     }
     mult = fact(current_num);
     printf("%lld - factorial of %d.\n", mult, current_num);
@@ -228,7 +259,6 @@ int main(int argc,char* argv[]) {
   default:
     break;
   }
-
   
   return 0;
 }
