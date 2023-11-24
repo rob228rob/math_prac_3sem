e91ea9f9a30051fdbb140b6dea4d755ac7948fa7
rob228rob <rob228rob@gmail.com>
2023-11-24 18:24:25 +0300

    last vers of 2_8

--- MP_3sem/Lab2/Lab2_8.c
+++ MP_3sem/Lab2/Lab2_8.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <stdbool.h>
-#include <ctype.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <math.h>
 
-typedef struct {
-    double x;
-    double y;
-    int positive_cos;
-} vector;
 
 typedef enum {
     OK,
@@ -34,195 +28,180 @@ void responce(int status) {
     if (status == NOT_A_NUMBER) printf("NOT_A_NUMBER.\n");
 }
 
-STATUS to_upper(char symb, int* res) {
-    if (symb >= 'a' && symb <= 'z') {
-        *res = symb - 'a' + 'A';
-    } else {
-        *res = symb;
-    }
-}
+STATUS sum_in_base(char** result, int base, int count_number, ...);
 
-STATUS convert_to_decimal(char number[], int base, int* _decimal) {
-    if (base == 10) {
-        *_decimal = atoi(number);
-        return OK;
-    }
-    int decimal = 0;
-    int power = 1;
-    int length = strlen(number);
-    if (length > 8) {
-        return INVALID_DATA;
-    }
-    
-    for (int i = length - 2; i >= 0; i--) {
-        int digit;
-        if (number[i] >= '0' && number[i] <= '9') {
-            digit = number[i] - '0';
-        } else if (number[i] >= 'A' && number[i] <= 'Z') {
-            digit = number[i] - 'A' + 11;
-        }
-        decimal += digit * power;
-        power *= base;
-    }
-    
-    *_decimal = decimal;
-    return OK;
-}
+int check_valid(int base, char** number);
 
-STATUS convert_from_decimal(int decimalNumber, int base, char** num) {
-    char convertedNumber[100];
-    int index = 0;
-    
-    while (decimalNumber != 0) {
-        int remainder = decimalNumber % base;
-        
-        if (remainder >= 0 && remainder <= 9) {
-            convertedNumber[index] = remainder + '0';
-        } else if (remainder >= 10 && remainder <= 35) {
-            convertedNumber[index] = remainder - 10 + 'A';
-        }
-        
-        decimalNumber /= base;
-        index++;
-    }
-    *num = (char*)malloc(sizeof(char)*(index+1));
-    if (*num == NULL) {
-        return MEMORY_ERROR;
-    }
+void reverse(char** number, int len);
 
-    strcpy((*num),convertedNumber);
-    return OK;
-}
-STATUS is_flag(int symb) {
-    char *flags = "lrnuc";
-    for (int i = 0; i < 5; ++i) {
-        if (flags[i] == symb) {
-            return OK;
-        }
-    } 
-    return UNDEFINED_BEHAVIOR;
-}
+int is_valid_char(int ch);
 
-// return length of str
-int get_length(char* str) {
-    if (str == NULL) {
-        return 0;
-    }
-    
-    int length = 0;
-    while (*(str + length) != '\0') {
-        length++;
-    }
-    
-    return length;
-}
+void sum_of_numbers(char** result, char* number, int base, int* len_res, int len_num);
 
+int main() {
+    char* res;
+    int count = 3;
+    int base = 10;
+    char* num1 = "1"; 
+    char* num2 = "99999999998"; 
+    char* num3 = "1"; 
 
-void swap(char *a, char *b) {
-    char temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-STATUS string_cpy(char* source_str, char* str_cpy, int length) {
-    if (str_cpy == NULL || source_str == NULL) {
-        return MEMORY_ERROR;
-    }
-    for (int i = 0; i < length; ++i) {
-        str_cpy[i] = source_str[i];
-    }
-    str_cpy[length] = '\0';
-    return OK;
-}
-
-STATUS power(double base, int exponent, double *result) {
-    if(exponent < 0) {
-        return INVALID_DATA;
-    }
-    else if(exponent == 0) {
-        *result = 1;
-        return OK;
-    }
-    else if(exponent % 2 == 0) {
-        STATUS exp2 = power(base * base, exponent / 2, result);
+    STATUS summ_status = sum_in_base(&res, base, count, num1, num2, num3);
+    if(summ_status == OK) {
+        printf("summ: %s\n", res);
     }
     else {
-        STATUS exp = power(base, exponent - 1, result);
-        *result *= base;
+        responce(summ_status);
+        return 1;
     }
-    return OK;
+    free(res);
+
+    return 0;
 }
 
-
-
-int char_to_number(char c) {
-    if (isdigit(c)) {
-        return c - '0';
-    } 
-    else {
-        return toupper(c) - 'A' + 10;
-    }
-}
-
-char number_to_char(int num) {
-    if (num < 10 || num >= 0) {
-        return num + '0';
-    }
-    else {
-        return num - 10 + 'A';
-    }
-}
-
-// Summ number in the num syst with current base
-STATUS sum_by_numeral_syst(int *_result, int base, int count, ...) {
-    
-    if (base < 2 || base > 36) {
-        return INVALID_DATA; 
-    }
-    if (count <= 0) {
+STATUS sum_in_base(char ** result, int base, int count_number, ...)
+{
+    if(count_number <= 0 || (base < 2 || base > 36))
+    {
         return INVALID_DATA;
     }
 
     va_list args;
-    va_start(args, count);
+    va_start(args, count_number);
 
-    int result = 0;
-    for (int i = 0; i < count; i++) {
-        char* num = va_arg(args, char*);
-        int temp_int;
-        STATUS status = convert_to_decimal(num, base, &temp_int);
-        if (status != OK) {
-            return status;
-        }
-        result += temp_int;
+    char * number_arg = va_arg(args, char*);
+    char * number = strdup(number_arg);
+    int len_num = strlen(number);
+    if(check_valid(base, &number)) {
+        va_end(args);
+        free(number);
+        return INVALID_DATA;
     }
-    
+    reverse(&number, len_num);
+
+    int capacity_res = len_num * 2, len_res = len_num;
+    (*result) = (char *)calloc(sizeof(char), capacity_res);
+    if((*result) == NULL) {
+        va_end(args);
+        free(number);
+        return INVALID_DATA;
+    }
+    strcpy(*result, number);
+    free(number);
+
+    for (int i = 0; i < count_number - 1; ++i) {
+        number_arg = va_arg(args, char*);
+        number = strdup(number_arg);
+        len_num = strlen(number);
+        if(check_valid(base, &number)) {
+            va_end(args);
+            free(number);
+            return INVALID_DATA;
+        }
+        reverse(&number, len_num);
+        if(len_num + 1 > capacity_res || len_res + 1 > capacity_res) {
+            int max = len_res > len_num ? len_res : len_num;
+            capacity_res = max * 2;
+            char * tmp = (char *)realloc((*result), capacity_res * sizeof(char));
+            if (tmp == NULL) {
+                va_end(args);
+                free(number);
+                return MEMORY_ERROR;
+            }
+            (*result) = tmp;
+        }
+
+        sum_of_numbers(result, number, base, &len_res, len_num);
+        free(number);
+    }
+    while(len_res > 1 && (*result)[len_res - 1] == '0')
+    {
+        (*result)[--len_res] = '\0';
+    }
+    reverse(result, len_res);
     va_end(args);
-    
-    (*_result) = result;
     return OK;
 }
 
-
-
-int main(int argc, char* argv[]) {
-
-    int base = 2; 
-    int count = 3; 
-    char* num1 = "01"; 
-    char* num2 = "01"; 
-    char* num3 = "01"; 
-    int result = -1;
-    STATUS sum_stat = sum_by_numeral_syst(&result,base, count, num1, num2, num3);
-    if (sum_stat != OK) {
-        responce(sum_stat);
+int check_valid(int base, char ** number)
+{
+    if((*number)[0] == '\0')
+    {
         return 1;
     }
-    //char* result = addNumbers(base, count, num1, num2, num3);
-    
-    printf("Результат: %d\n", result);
-    
-    //free(result); // Освобождение памяти
-    
+    for(int i = 0; (*number)[i] != '\0'; ++i)
+    {
+        if(isdigit((*number)[i]))
+        {
+            if( (*number)[i] - '0'>= base)
+            {
+                return 1;
+            }
+        }
+        else if(isalpha((*number)[i]))
+        {
+            (*number)[i] = (char)toupper((*number)[i]);
+            if((*number)[i]  - 'A' + 10 >= base)
+            {
+                return 1;
+            }
+        }
+        else
+        {
+            return 1;
+        }
+    }
     return 0;
+}
+
+void reverse(char ** number, int len)
+{
+    int i = 0, j = len - 1;
+    for(; (j - i) > 0; ++i, --j)
+    {
+        char temp = (*number)[i];
+        (*number)[i] = (*number)[j];
+        (*number)[j] = temp;
+    }
+}
+
+void sum_of_numbers(char ** result, char * number, int base, int * len_res, int len_num)
+{
+    int len_sum = (*len_res > len_num) ? len_num : *len_res;
+    int next_digit = 0;
+    int idx = 0;
+    for(; idx < len_sum; ++idx)
+    {
+        int first_number = isdigit((*result)[idx]) ? (*result)[idx] - '0' : (*result)[idx] - 'A' + 10;
+        int second_number = isdigit(number[idx]) ? number[idx] - '0' : number[idx] - 'A' + 10;
+        int sum = first_number + second_number  + next_digit;
+        (*result)[idx] = (sum % base) > 9 ? (sum % base) + 'A' - 10 : (sum % base) + '0';
+        next_digit = sum / base;
+    }
+    if(*len_res > len_num)
+    {
+        for(; idx < *len_res; ++idx)
+        {
+            int first_number = isdigit((*result)[idx]) ? (*result)[idx] - '0' : (*result)[idx] - 'A' + 10;
+            int sum = first_number + next_digit;
+            (*result)[idx] = (sum % base) > 9 ? (sum % base) + 'A' - 10 : (sum % base) + '0';
+            next_digit = sum / base;
+        }
+    }
+    if(len_num > *len_res)
+    {
+        for(; idx < len_num; ++idx)
+        {
+            int second_number = isdigit(number[idx]) ? number[idx] - '0' : number[idx] - 'A' + 10;
+            int sum = second_number + next_digit;
+            (*result)[idx] = (sum % base) > 9 ? (sum % base) + 'A' - 10 : (sum % base) + '0';
+            next_digit = sum / base;
+        }
+    }
+    if(next_digit != 0)
+    {
+        (*result)[idx++] = next_digit > 9 ? next_digit + 'A' - 10 : next_digit + '0';
+    }
+    (*result)[idx] = '\0';
+    *len_res = idx;
 }
